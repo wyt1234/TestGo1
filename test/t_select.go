157bbe7fa8d5a81e163d1b2e3c7ff11562e7de95
wyt1234 <42131304+wyt1234@users.noreply.github.com>
2022-11-14 14:29:20 +0800

    t_select.go

---
+++ test/t_select.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// https://blog.csdn.net/qq_41078889/article/details/123908782
+func main() {
+	stop := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-stop:
+				fmt.Println("监控退出，停止了...")
+				return
+			default:
+				fmt.Println("goroutine监控中...")
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}()
+	time.Sleep(10 * time.Second)
+	fmt.Println("可以了，通知监控停止")
+	stop <- true
+	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+	close(stop)
+	time.Sleep(5 * time.Second)
+}
